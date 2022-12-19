6cc3dc0faa003360d4f9a5e2ffb990e6b912752b
jackiezkl <jackiezkl@users.noreply.github.com>
2022-12-18 21:23:24 -0600

    Update adding_challenge.py

--- adding_challenge.py
+++ adding_challenge.py
@@ -3,14 +3,8 @@ import sys
 
 
 def main():
-    try:
-        url = sys.argv[1]
-        token = sys.argv[2]
-    except IndexError:
-        print("Usage: python3 add_user.py <url> <admin_token>")
-        sys.exit(1)
-
-    # Create API Session
+    token = "4fb4c02d643f6667f2d187eb62c081f3b1e0e987978b896d9c1f4ab557db285f"
+    url = http://209.114.126.63
     url = url.strip("/")
     s = requests.Session()
     s.headers.update({"Authorization": f"Token {token}"})
@@ -18,9 +12,8 @@ def main():
     # NOTE: If you wish for the user's credentials to be emailed to them, pass the
     # notify=true parameter in the URL. For example: /api/v1/users?notify=true
     r = s.post(
-        f"{url}/api/v1/users",
-        json={"name":"user","email":"user@sampleuser.com","password":"user","type":"user","verified":False,"hidden":False,"banned":False,"fields":[]},
-        headers={"Content-Type": "application/json"},
+        f"{url}/api/v1/challenges",
+        json={"name":"XOR Challenge 2","category":"Coordination","description":"Retrieve \"**Tom**\" and \"**Ashley**\"'s secret  8-bit number\r\n\r\nReturn the XOR of these two binary sequences.\r\n\r\nThe flag is in the format:``flag{01010101}``\r\n\r\nplease use private one-on-one chat function.","value":"23","state":"hidden","type":"standard"},
     )
     print(r.json())
 
