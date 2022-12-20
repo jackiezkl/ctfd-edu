877a6e5d6aaf507a0a8759e84903e66929281827
jackiezkl <jackiezkl@users.noreply.github.com>
2022-12-20 12:05:25 -0600

    Update adding_challenge.py

--- adding_challenge.py
+++ adding_challenge.py
@@ -2,39 +2,39 @@ import requests
 import sys
 import json
 
-def main(url,token):
+def get_usernames(url,token):
 
-    s = requests.Session()
-    s.headers.update({"Authorization": f"Token {token}"})
 
-#     r = s.post(
-#         f"{url}/api/v1/challenges",
-#         json={"name":"XOR Challenge 3",
-#                "category":"Coordination",
-#                "description":"Retrieve \"**Tom**\" and \"**Ashley**\"'s secret  8-bit number\r\n\r\nReturn the XOR of these two binary sequences.\r\n\r\nThe flag is in the format:``flag{01010101}``\r\n\r\nplease use private one-on-one chat function.",
-#                "value":"24",
-#                "state":"visible",
-#                "type":"standard"},
-#     )
-
-#     s.post(
-#         f"{url}/api/v1/flags",
-#         json={"challenge_id":"4","content":"30101010","type":"static","data":""},
-#     )
     
-    r = s.get(
-        f"{url}/api/v1/users",
-    )
-    y = r.json()
+    y = s.get(f"{url}/api/v1/users").json()
 
     n = y['meta']['pagination']['total']
 
     for i in range(n):
-    # the result is a Python dictionary:
-
         print(y['data'][i]['name'])
+        
+
+        
+def add_new_challenge()
+    s = requests.Session()
+    s.headers.update({"Authorization": f"Token {token}"})
+    
+    r = s.post(
+        f"{url}/api/v1/challenges",
+        json={"name":"XOR Challenge 3",
+               "category":"Coordination",
+               "description":"Retrieve \"**Tom**\" and \"**Ashley**\"'s secret  8-bit number\r\n\r\nReturn the XOR of these two binary sequences.\r\n\r\nThe flag is in the format:``flag{01010101}``\r\n\r\nplease use private one-on-one chat function.",
+               "value":"24",
+               "state":"visible",
+               "type":"standard"},
+    )
+
+    s.post(
+        f"{url}/api/v1/flags",
+        json={"challenge_id":"4","content":"30101010","type":"static","data":""},
+    )
 
 if __name__ == "__main__":
     token = "4fb4c02d643f6667f2d187eb62c081f3b1e0e987978b896d9c1f4ab557db285f"
     url = "http://209.114.126.63"    
-    main(url,token)
+    get_usernames(url,token)
