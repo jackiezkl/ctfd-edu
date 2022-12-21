1678cb7f5cc7dbb33f09b7db3872ccaee42a194b
jackiezkl <jackiezkl@users.noreply.github.com>
2022-12-21 09:54:27 -0600

    Update adding_challenge.py

--- adding_challenge.py
+++ adding_challenge.py
@@ -1,12 +1,42 @@
-import requests,sys,json,csv
+import requests,sys,json,csv,random
+
+def generate_binary():
+  hex_array = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+
+  i = random.randint(0,15)
+  j = random.randint(0,15)
+  first_binary = (bin(int(hex_array[i], 16))[2:].zfill(4))
+  second_binary = (bin(int(hex_array[j], 16))[2:].zfill(4))
+  xor_result = (bin(int(hex_array[i], 16) ^ int(hex_array[j], 16))[2:].zfill(4))
+  return first_binary
+
+# def create_code_assign_record():
+#   csv = open('code_assign_record.csv', 'w', newline='')
+
+#   csv.write('Name,code\n')
+#   print("[+] Created code assign record file: code_assign_record.csv")
+#   csv.close()
+
+def create_xor_record():
+  csv = open('xor_record.csv', 'w', newline='')
+
+  csv.write('Name1,code1,name2,code2,XOR\n')
+  print("[+] Created XOR record file: xor_record.csv")
+  csv.close()
 
 def get_usernames(url):
+  csv = open('code_assign_record.csv', 'w', newline='')
+
+  csv.write('Name,code\n')
+  print("[+] Created code assign record file: code_assign_record.csv")
+  
   username_session = requests.Session()
   all_user_info_json = username_session.get(f"{url}/api/v1/users").json()
   total_number_of_users = all_user_info_json['meta']['pagination']['total']
 
   for i in range(total_number_of_users):
-    print(all_user_info_json['data'][i]['name'])
+    csv.write(all_user_info_json['data'][i]['name'],generate_binary())
+
 
 
         
@@ -31,6 +61,10 @@ def add_new_challenge(url,token):
 
 if __name__ == "__main__":
   token = "4fb4c02d643f6667f2d187eb62c081f3b1e0e987978b896d9c1f4ab557db285f"
-  url = "http://209.114.126.63"    
+  url = "http://209.114.126.63"
+
+#   create_code_assign_record()
+#   create_xor_record()
+
   get_usernames(url)
 #   add_new_challenge(url,token)
