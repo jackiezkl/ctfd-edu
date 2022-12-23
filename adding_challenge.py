94c8498f4f6d44e88b115a9c56f901cff5732533
jackiezkl <jackiezkl@users.noreply.github.com>
2022-12-23 17:56:27 -0600

    Update adding_challenge.py

--- adding_challenge.py
+++ adding_challenge.py
@@ -25,7 +25,7 @@ def update_user_profile(url,token):
 #       user_update_session.headers.update({"Authorization": f"Token {token}"})
       payload = '{"name":"'+line[1]+'","email":"'+line[2]+'","type":"'+line[3]+'","verified":false,"hidden":false,"banned":false,"fields":[{"field_id":1,"value":"'+line[8]+'"},{"field_id":2,"value":"'+line[10]+'"},{"field_id":3,"value":"'+bin(int(line[11], 16))[2:].zfill(8)+'"}]}'
       r = user_update_session.patch(
-        f"{url}/api/v1/users/{line[0]}",
+        f"{url}/api/v1/users/{line[0].toInt}",
         json=f"{payload}",
         headers={"Content-Type": "application/json"},
       )
