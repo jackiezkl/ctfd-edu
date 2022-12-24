fdd3e7c6345f561916aa393e8077898e548badc5
jackiezkl <jackiezkl@users.noreply.github.com>
2022-12-24 10:31:11 -0600

    Update adding_challenge.py

--- adding_challenge.py
+++ adding_challenge.py
@@ -22,7 +22,9 @@ def update_user_profile(url,token):
     user_update_session.headers.update({"Authorization": f"Token {token}"})
     for line in users_reader:
       user_id = int(line[0])
-      payload = '{"name":"'+line[1]+'","email":"'+line[2]+'","type":"user","verified":false,"hidden":false,"banned":false,"fields":[{"field_id":1,"value":"'+line[3]+'"},{"field_id":2,"value":"'+line[4]+'"},{"field_id":3,"value":"'+bin(int(line[5], 16))[2:].zfill(8)+'"}]}'
+#       payload = '{"name":"'+line[1]+'","email":"'+line[2]+'","type":"user","verified":false,"hidden":false,"banned":false,"fields":[{"field_id":1,"value":"'+line[3]+'"},{"field_id":2,"value":"'+line[4]+'"},{"field_id":3,"value":"'+bin(int(line[5], 16))[2:].zfill(8)+'"}]}'
+      payload = '{"fields":[{"field_id":3,"value":"'+bin(int(line[5], 16))[2:].zfill(8)+'"}]}'
+
       user_update_session.patch(
         f"{url}/api/v1/users/{user_id}",
         json=json.loads(payload),
