720847c853d777e94a68d27e62c6b6506d8d736d
jackiezkl <jackiezkl@users.noreply.github.com>
2022-12-29 20:40:13 -0600

    Create run_first.py

---
+++ run_first.py
@@ -0,0 +1,9 @@
+if __name__="__main__":
+  with open('CTFd/themes/core/templates/.txt', 'r+') as f: 
+    lines = f.readlines()
+    for i, line in enumerate(lines):
+        if line.startswith('<meta name="viewport"'):
+            lines[i] = lines[i].strip() + '\n<meta name="starts_in" content="{{ ctf_starts_in() }}">\n<meta name="ends_in" content="{{ ctf_ends_in() }}">'
+    f.seek(0)
+    for line in lines:
+        f.write(line)
