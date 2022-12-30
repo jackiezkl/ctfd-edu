59507dcdc5453c3b45961d5861513606a2ace2ea
jackiezkl <jackiezkl@users.noreply.github.com>
2022-12-29 21:14:07 -0600

    Update run_first.py

--- run_first.py
+++ run_first.py
@@ -9,7 +9,7 @@ def main():
       f.write(line)
     f.close()
     
-  with open(('/home/ctfd/Desktop/test/CTFd/CTFd/themes/core/templates/base.html', 'r+') as base_file:
+  with open('/home/ctfd/Desktop/test/CTFd/CTFd/themes/core/templates/base.html', 'r+') as base_file:
     search_text = '<a href="https://ctfd.io" class="text-secondary">\n\t\t\t\t<small class="text-muted">Powered by CTFd</small>\n\t\t\t</a>\n'
     replace_text = '<script>const d=new Date();let year=d.getFullYear();document.write("CTF - "+year);</script>'
 
