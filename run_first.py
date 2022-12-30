fc09fad3ec71ce5c4ed9c34907c2d62b36c65402
jackiezkl <jackiezkl@users.noreply.github.com>
2022-12-29 21:53:19 -0600

    Update run_first.py

--- run_first.py
+++ run_first.py
@@ -10,7 +10,7 @@ def main():
     f.close()
     
   with open('/home/ctfd/Desktop/test/CTFd/CTFd/themes/core/templates/base.html', 'r') as base_file:
-    search_text = '<a href="https://ctfd.io" class="text-secondary">'#\n\t\t\t\t<small class="text-muted">Powered by CTFd</small>\n\t\t\t</a>\n'
+    search_text = '<a href="https://ctfd.io" class="text-secondary">\n\t\t\t\t<small class="text-muted">Powered by CTFd</small>\n\t\t\t</a>\n'
     replace_text = '<script>const d=new Date();let year=d.getFullYear();document.write("CTF - "+year);</script>'
     data = base_file.read()
     data = data.replace(search_text, replace_text)
