7eb5435a9c62523eb786f2fc4a33ae9687bfb52c
jackiezkl <jackiezkl@users.noreply.github.com>
2022-12-29 21:11:41 -0600

    Update run_first.py

--- run_first.py
+++ run_first.py
@@ -1,9 +1,23 @@
-if __name__=="__main__":
-  with open('CTFd/themes/core/templates/base.html', 'r+') as f: 
+def main():
+  with open('/home/ctfd/Desktop/test/CTFd/CTFd/themes/core/templates/base.html', 'r+') as f: 
     lines = f.readlines()
     for i, line in enumerate(lines):
-        if line.startswith('<meta name="viewport"'):
-            lines[i] = lines[i].strip() + '\n<meta name="starts_in" content="{{ ctf_starts_in() }}">\n<meta name="ends_in" content="{{ ctf_ends_in() }}">'
+      if line.startswith('\t<meta name="viewport"'):
+        lines[i] = lines[i].strip() + '\n\t<meta name="starts_in" content="{{ ctf_starts_in() }}">\n\t<meta name="ends_in" content="{{ ctf_ends_in() }}">'
     f.seek(0)
     for line in lines:
-        f.write(line)
+      f.write(line)
+    f.close()
+    
+  with open(('/home/ctfd/Desktop/test/CTFd/CTFd/themes/core/templates/base.html', 'r+') as file:
+    search_text1 = '<a href="https://ctfd.io" class="text-secondary">\n\t\t\t\t<small class="text-muted">Powered by CTFd</small>\n\t\t\t</a>\n'
+    replace_text = '<script>const d=new Date();let year=d.getFullYear();document.write("CTF - "+year);</script>'
+
+    data = file.read()
+  
+    data = data.replace(search_text, replace_text)
+  
+    file.write(data)
+
+if __name__=="__main__":
+  main()
