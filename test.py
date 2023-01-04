1b7919e901aa1d903218d1a15d24a7d0be65da06
jackiezkl <jackiezkl@users.noreply.github.com>
2023-01-04 15:58:45 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,21 @@
 import csv
 
-def does_challenge_exist(url,token):
+def does_challenge_exist():
   flag = 0
 
-  check_existence = requests.Session()
+  with requests.Session() as check_existence:
   check_existence.headers.update({"Authorization": f"Token {token}"})
-  challenge_result = check_existence.get(f"{url}/api/v1/challenges",json='').json()
+  challenges_list = check_existence.get(f"{url}/api/v1/challenges",json='').json()
 
-  for name in challenge_result['data']:
-    if name['name'] == "Birth Month 2":
-      check_existence.close()
+  for challenge in challenges_list['data']:
+    if challenge['name'] == "Birth Month 2":
       return 2
-    elif name['name'] == "Birth Month 1":
-      check_existence.close()
+    elif challenge['name'] == "Birth Month 1":
       return 1
     else:
-      check_existence.close()
       return 0
 
-def birthmonth_challenge(url,token,number_of_exist_challenge):
+def birthmonth_challenge(number_of_exist_challenge):
   with open("users_info_record.csv") as users_info_record:
     user_info_dictreader = csv.DictReader(users_info_record)
     ids=[]
@@ -61,11 +58,11 @@ def birthmonth_challenge(url,token,number_of_exist_challenge):
         pass
 
     with open("birth_month_record.csv",'a') as birth_month_record:
-      col_names = ['full_name','birth_month','challenge_exist','challenge_number']
+      col_names = ['first_name','birth_month','challenge_exist','challenge_number']
       birth_month_writer = csv.DictWriter(birth_month_record, fieldnames=col_names)
 
-    if does_challenge_exist(url,token) == 1:
-      if add_new_challenge(url,token,picked_id,picked_full_name,picked_birth_month) is True:
+    if does_challenge_exist() == 1:
+      if add_new_challenge(picked_id,picked_full_name,picked_birth_month,2) is True:
         row="{'id':'"+ids[n]+"', 'user_name':'"+full_name[n]+"','user_hex':'"+user_hex[n]+"','paired_name':'"+paired_name[n]+"','paired_hex':'"+paired_hex[n]+"','xor_result':'"+xor_result[n]+"','challenge_exist':'yes','challenge_number':'"+str(int(n)+1)+"'}"
         row_dict = ast.literal_eval(row)
         birth_month_writer.writerow(row_dict)
@@ -79,7 +76,7 @@ def birthmonth_challenge(url,token,number_of_exist_challenge):
     
   else:
 
-def get_last_created_id(url,token,n):
+def get_last_created_id(,n):
   id_check_session = requests.Session()
   id_check_session.headers.update({"Authorization": f"Token {token}"})
   id_check_result = id_check_session.get(f"{url}/api/v1/challenges",headers={"Content-Type": "application/json"}).json()
@@ -91,7 +88,7 @@ def get_last_created_id(url,token,n):
       pass
 
 # add new birth month challenges
-def add_new_birth_challenge(url,token,picked_id,picked_full_name,picked_birth_month,n):
+def add_new_birth_challenge(picked_id,picked_full_name,picked_birth_month,n):
   update_session = requests.Session()
   update_session.headers.update({"Authorization": f"Token {token}"})
   payload = '{"name":"Birth Month "'+n+',"category":"Coordination","description":"There should be at least one player that was born in '+picked_birth_month+'. Could you provide the first name of at least one?\\r\\nEnter their first name as the flag.","value":"34","state":"visible","type":"standard"}'
@@ -99,13 +96,13 @@ def add_new_birth_challenge(url,token,picked_id,picked_full_name,picked_birth_mo
   add_challenge_result = challenge_result['success']
   update_session.close()
 
-  last_id = get_last_created_id(url,token,n)
+  last_id = get_last_created_id(n)
 
-  result = add_new_birth_flag(url,token,last_id,picked_full_name,picked_birth_month)
+  result = add_new_birth_flag(last_id,picked_full_name,picked_birth_month,add_challenge_result)
   return result
 
 # add corresponding birth month flags
-def add_new_birth_flag(url,token,last_id,url,token,last_id,picked_full_name,picked_birth_month):
+def add_new_birth_flag(last_id,last_id,picked_full_name,picked_birth_month,add_challenge_result):
   update_session = requests.Session()
   update_session.headers.update({"Authorization": f"Token {token}"})
   content_string = ''
@@ -121,15 +118,18 @@ def add_new_birth_flag(url,token,last_id,url,token,last_id,picked_full_name,pick
     flag_result = update_session.post(f"{url}/api/v1/flags",json=json.loads(payload)).json()
 
     if flag_result['success'] == True:
+      update_session.close()
       print("[+] New birth month challenge and flag added.")
       return True
     else:
+      update_session.close()
       print("[+] Error when adding flag.")
       return False
   else:
+    update_session.close()
     print("[+] Error when adding challenge.")
     return False
-  update_session.close()
+  
 
 if __name__ == "__main__":
   token = "3faf06e19cc198608a2aa9c5ee1f736f93f6c29e8f92bd633dfc4b3af5900e96"
@@ -137,7 +137,7 @@ if __name__ == "__main__":
 
   if os.path.isfile('birth_month_record.csv') == False:
       with open("birth_month_record.csv",'w',newline='') as birth_month_record:
-        col_names = ['full_name','birth_month','challenge_exist','challenge_number']
+        col_names = ['first_name','birth_month','challenge_exist','challenge_number']
         writer = csv.DictWriter(birth_month_record, fieldnames=col_names)
 
         writer.writeheader()
@@ -145,11 +145,9 @@ if __name__ == "__main__":
   else:
     pass
 
-  number_of_exist_challenge = does_challenge_exist(url,token)
+  number_of_exist_challenge = does_challenge_exist()
 
   if number_of_exist_challenge < 2:
-    birthmonth_challenge(url,token,number_of_exist_challenge)
+    birthmonth_challenge(number_of_exist_challenge)
   else:
     exit()
-
-
