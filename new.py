378bae7614011b948d6bda09e082c2e7b96ede62
jackiezkl <jackiezkl@users.noreply.github.com>
2023-01-22 16:03:16 -0600

    Create new.py

---
+++ new.py
@@ -0,0 +1,8 @@
+def update_points():
+  print('done')
+
+if __name__ == "__main__":
+  token = "e893233cbc79e4b567bd118f24758f44c4da7c0cf671900abac4ca73e83b3986"
+  url = "http://209.114.126.52"
+
+  update_points()
