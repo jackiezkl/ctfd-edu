272a0a0a225f8c04723a0ab206fc103c084382f9
jackiezkl <matins.fluting_0t@icloud.com>
2023-01-23 15:06:06 -0600

    modified:   new.py

--- new.py
+++ new.py
@@ -1,3 +1,20 @@
+import requests,json
+
+def count_coordination():
+  count = 0
+  with requests.Session() as check_existence:
+    check_existence.headers.update({"Authorization": f"Token {token}"})
+    challenge_result = check_existence.get(f"{url}/api/v1/challenges",json='').json()
+    # print(n)
+    for name in challenge_result['data']:
+      if "XOR Challenge" in name['name']:
+        count+=1
+      elif "Birth Month" in name['name']:
+        count+=1
+
+  print(count)
+  update_points()
+
 def update_points():
   print('done')
 
@@ -5,4 +22,4 @@ if __name__ == "__main__":
   token = "099b06d2394093117dfd53ca9e01f23a9437fda5579bdd82a317861740f1b35f"
   url = "http://209.114.126.86"
 
-  update_points()
+  count_coordination()
