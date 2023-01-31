cc90c189547d922cc9406132f7d03c68c10aa916
jackiezkl <matins.fluting_0t@icloud.com>
2023-01-31 11:32:42 -0600

    update

--- main.py
+++ main.py
@@ -506,41 +506,19 @@ def update_points(challenge_id,new_points):
       print('[e] Challenge '+challenge_id+' points not updated')
 
 ##--------------the section below update the prerequisite for new and existing challenges-------------- 
-def patch_prereq_name(challenge_name,prereq_name):
-  prerequisites_id = ''
-  challenge_id = ''
-
-  with requests.Session() as check_existence:
-    check_existence.headers.update({"Authorization": f"Token {token}"})
-    challenge_result = check_existence.get(f"{url}/api/v1/challenges",json='').json()
-
-    for name in challenge_result['data']:
-      if challenge_name in name['name']:
-        challenge_id = str(name['id'])
-
-    for name in challenge_result['data']:
-      if prereq_name in name['name']:
-        prerequisites_id = str(name['id'])
-
+def check_req(challenge_id):
+  with requests.Session() as update_session:
+    update_session.headers.update({"Authorization": f"Token {token}"})
+    flag_result = update_session.get(f"{url}/api/v1/challenges/{challenge_id}/requirements",json='').json()
     try:
-      with requests.Session() as update_session:
-        update_session.headers.update({"Authorization": f"Token {token}"})
-        payload = '{"requirements":{"prerequisites":['+prerequisites_id+']}}'
-        flag_result = update_session.patch(f"{url}/api/v1/challenges/{challenge_id}",json=json.loads(payload))
+      requirements_id = str(flag_result['data']['prerequisites'][0])
+      # print(str(challenge_id) + ':'+requirements_id)
+      return requirements_id
     except Exception:
-      print("[e] Something's wrong, nothing changed.")
+      # print(challenge_id)
+      return ''
 
-def patch_prereq_id(challenge_name,prereq_id):
-  challenge_id = ''
-
-  with requests.Session() as check_existence:
-    check_existence.headers.update({"Authorization": f"Token {token}"})
-    challenge_result = check_existence.get(f"{url}/api/v1/challenges",json='').json()
-
-    for name in challenge_result['data']:
-      if challenge_name in name['name']:
-        challenge_id = str(name['id'])
-
+def patch_prereq(challenge_id,prereq_id):
     try:
       with requests.Session() as update_session:
         update_session.headers.update({"Authorization": f"Token {token}"})
@@ -549,123 +527,89 @@ def patch_prereq_id(challenge_name,prereq_id):
     except Exception:
       print("[e] Something's wrong, nothing changed.")
 
-def patch_prereq_reverseid(challenge_id,prereq_name):
-  prerequisites_id = ''
-  with requests.Session() as check_existence:
-    check_existence.headers.update({"Authorization": f"Token {token}"})
-    challenge_result = check_existence.get(f"{url}/api/v1/challenges",json='').json()
-
-    for name in challenge_result['data']:
-      if prereq_name in name['name']:
-        prerequisites_id = str(name['id'])
-
-  try:
-    with requests.Session() as update_session:
-      update_session.headers.update({"Authorization": f"Token {token}"})
-      payload = '{"requirements":{"prerequisites":['+prerequisites_id+']}}'
-      flag_result = update_session.patch(f"{url}/api/v1/challenges/{challenge_id}",json=json.loads(payload))
-  except Exception:
-    print("[e] Something's wrong, nothing changed.")
-
-def check_name(challenge_name):
-  with requests.Session() as check_existence:
-    check_existence.headers.update({"Authorization": f"Token {token}"})
-    challenge_result = check_existence.get(f"{url}/api/v1/challenges",json='').json()
-
-    for name in challenge_result['data']:
-      if challenge_name in name['name']:
-        return True
-
-# given a challenge id, change its visibility to visible
 def make_visible(challenge_id):
   with requests.Session() as update_session:
     update_session.headers.update({"Authorization": f"Token {token}"})
     payload = '{"state":"visible"}'
     flag_result = update_session.patch(f"{url}/api/v1/challenges/{challenge_id}",json=json.loads(payload))
 
-
 def patch_new_prereq():
-  if check_name("XOR Challenge 8") == True:
-    patch_prereq_id("XOR Challenge 8","35")
-  else:
-    pass
+  challenge_dict = {}
+  cid = 82
+  while True:
+    try:
+      with requests.Session() as check_existence:
+        check_existence.headers.update({"Authorization": f"Token {token}"})
+        challenge_result = check_existence.get(f"{url}/api/v1/challenges/{cid}",json='').json()
+        # print(challenge_result)
+        challenge_id = str(challenge_result['data']['id'])
+        challenge_name = challenge_result['data']['name']
+        challenge_value = str(challenge_result['data']['value'])
+        challenge_category = challenge_result['data']['category']
+        challenge_prereq = check_req(challenge_id)
+        # print(challenge_id+": ['"+challenge_name+"','"+challenge_value+"','"+challenge_category+"','"+challenge_prereq+"'],")
+        challenge_dict[challenge_id]=[challenge_name,challenge_value,challenge_category,challenge_prereq]
+    except Exception:
+      break
+    cid+=1
 
-  if check_name("XOR Challenge 7") == True:
-    patch_prereq_id("XOR Challenge 7","44")
-  else:
-    pass
-
-  if check_name("XOR Challenge 4") == True:
-    patch_prereq_id("XOR Challenge 4","64")
-  else:
-    pass
-
-  if check_name("Birth Month 2") == True:
-    patch_prereq_id("Birth Month 2","38")
-  else:
-    pass
-
-### 31 Linux 5 must be hidden at the beginning.
-  if check_name("XOR Challenge 5") == True:
-    patch_prereq_reverseid("31","XOR Challenge 5")
-    make_visible(31)
-  else:
-    pass
-
-  if check_name("XOR Challenge 5") == True:
-    patch_prereq_id("XOR Challenge 5","22")
-  else:
-    pass
-
-  if check_name("XOR Challenge 6") == True:
-    patch_prereq_reverseid("11","XOR Challenge 6")
-    make_visible(11)
-  else:
-    pass
-
-  if check_name("XOR Challenge 6") == True:
-    patch_prereq_id("XOR Challenge 6","22")
-  else:
-    pass
-
-  if check_name("Breakout Room 1") == True and check_name("XOR Challenge 1") == True:
-    patch_prereq_name("Breakout Room 1","XOR Challenge 1")
-  else:
-    pass
-
-  if check_name("XOR Challenge 2") == True:
-    patch_prereq_reverseid("71","XOR Challenge 2")
-    make_visible(71)
-  else:
-    pass
-
-  if check_name("XOR Challenge 2") == True:
-    patch_prereq_reverseid("47","XOR Challenge 2")
-    make_visible(47)
-  else:
-    pass
-
-  if check_name("Breakout Room 2") == True and check_name("XOR Challenge 3") == True:
-    patch_prereq("Breakout Room 2","XOR Challenge 3")
-  else:
-    pass
-
-  if check_name("XOR Challenge 3") == True:
-    patch_prereq_reverseid("12","XOR Challenge 3")
-    make_visible(12)
-  else:
-    pass
-
-  if check_name("XOR Challenge 3") == True and check_name("Birth Month 1") == True:
-    patch_prereq_name("XOR Challenge 3","Birth Month 1")
-  else:
-    pass
-
-  if check_name("Birth Month 1") == True:
-    patch_prereq_id("Birth Month 1", "79")
-  else:
-    pass
-
+  birth_month1_id = ''
+  for key,value in challenge_dict.items():
+    if "Birth Month 1" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq(key,"79")
+        birth_month1_id = key
+        make_visible(key)
+    elif "XOR Challenge 8" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq(key,"35")
+        make_visible(key)
+    elif "XOR Challenge 7" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq(key,"44")
+        make_visible(key)
+    elif "XOR Challenge 4" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq(key,"64")
+        make_visible(key)
+    elif "Birth Month 2" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq(key,"38")
+        make_visible(key)
+    elif "XOR Challenge 5" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq("31",key)
+        make_visible (31)
+        patch_prereq(key,"22")
+        make_visible(key)
+    elif "XOR Challenge 6" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq("11",key)
+        make_visible(11)
+        patch_prereq(key,"22")
+        make_visible(key)
+    elif "XOR Challenge 1" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq("80",key)
+        make_visible(80)
+        patch_prereq(key,"77")
+        make_visible(key)
+    elif "XOR Challenge 2" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq("71",key)
+        make_visible(71)
+        patch_prereq("47",key)
+        make_visible (47)
+        patch_prereq(key,"77")
+        make_visible(key)
+    elif "XOR Challenge 3" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq("81",key)
+        make_visible(81)
+        patch_prereq("12",key)
+        make_visible(12)
+        patch_prereq(key,birth_month1_id)
+        make_visible(key)
 
 if __name__ == "__main__":
   token = "15c288f2a166e3cef2ebb182a007212e747947aae7ff55fe103bcbb7f1695e2a"
--- test.py
+++ test.py
@@ -1,122 +1,119 @@
 import json,requests
 
-def get_last_created_xor_id(n):
-  cname = 'XOR Challenge '+n
-  cid = 82
-  while True:
-    with requests.Session() as id_check_session:
-      id_check_session.headers.update({"Authorization": f"Token {token}"})
-      id_check_result = id_check_session.get(f"{url}/api/v1/challenges/{cid}",headers={"Content-Type": "application/json"}).json()
-      try:
-        if cname in id_check_result['data']['name']:
-          return id_check_result['data']['id']
-      except Exception:
-        pass
-
-      try:
-        if "The requested URL was not found on the server" in id_check_result['message']:
-          break
-      except Exception:
-        pass
-      cid+=1
-
-def get_last_created_birth_id(n):
-  cname = 'Birth Month '+n
-  cid = 82
-  while True:
-    with requests.Session() as id_check_session:
-      id_check_session.headers.update({"Authorization": f"Token {token}"})
-      id_check_result = id_check_session.get(f"{url}/api/v1/challenges/{cid}",headers={"Content-Type": "application/json"}).json()
-      try:
-        if cname in id_check_result['data']['name']:
-          return id_check_result['data']['id']
-      except Exception:
-        pass
-
-      try:
-        if "The requested URL was not found on the server" in id_check_result['message']:
-          break
-      except Exception:
-        pass
-      cid+=1
-
-def challenge_id_existance(challenge_type,n):
-  if challenge_type == "xor":
-    cname = 'XOR Challenge '+n
-  else:
-    cname = 'Birth Month '+n
-
-  cid = 82
-
-  while True:
-    with requests.Session() as id_check_session:
-      id_check_session.headers.update({"Authorization": f"Token {token}"})
-      id_check_result = id_check_session.get(f"{url}/api/v1/challenges/{cid}",headers={"Content-Type": "application/json"}).json()
-      try:
-        if cname in id_check_result['data']['name']:
-          return id_check_result['data']['id'],True
-      except Exception:
-          return 'None',False
-
-      cid+=1
-
-def count_coordination(number_of_breakout_room):
-  count = number_of_breakout_room
-  cid = 82
-  while True:
-    with requests.Session() as id_check_session:
-      id_check_session.headers.update({"Authorization": f"Token {token}"})
-      id_check_result = id_check_session.get(f"{url}/api/v1/challenges/{cid}",headers={"Content-Type": "application/json"}).json()
-      try:
-        if id_check_result['data']['category'] == 'Coordination':
-          count += 1
-      except Exception:
-        break
-      cid+=1
-  
-  new_points = round(500/count)
-  print(new_points)
-
-  cid2 = 82
-  try:
-    while True:
-      with requests.Session() as id_check_session:
-        id_check_session.headers.update({"Authorization": f"Token {token}"})
-        id_check_result = id_check_session.get(f"{url}/api/v1/challenges/{cid2}",headers={"Content-Type": "application/json"}).json()
-        try:
-          if id_check_result['data']['category'] == 'Coordination':
-            update_points(str(id_check_result['data']['id']),new_points)
-        except Exception:
-          break
-        cid2+=1
-    print('[+] Coordination points changed')
-  except Exception:
-    print('[e] Coordination points not updated.')
-    pass
-    # try:
-    #   for name in challenge_result['data']:
-    #     if "XOR Challenge" in name['name']:
-    #       update_points(str(name['id']),new_points)
-    #     elif "Birth Month" in name['name']:
-    #       update_points(str(name['id']),new_points)
-    #   print('[+] Coordination points changed')
-    # except Exception:
-    #   print('[e] Coordination points not updated.')
-    #   pass
-
-def update_points(challenge_id,new_points):
+def check_req(challenge_id):
   with requests.Session() as update_session:
     update_session.headers.update({"Authorization": f"Token {token}"})
-    payload = '{"value":"'+str(new_points)+'"}'
+    flag_result = update_session.get(f"{url}/api/v1/challenges/{challenge_id}/requirements",json='').json()
+    try:
+      requirements_id = str(flag_result['data']['prerequisites'][0])
+      # print(str(challenge_id) + ':'+requirements_id)
+      return requirements_id
+    except Exception:
+      # print(challenge_id)
+      return ''
+
+def patch_prereq(challenge_id,prereq_id):
+    try:
+      with requests.Session() as update_session:
+        update_session.headers.update({"Authorization": f"Token {token}"})
+        payload = '{"requirements":{"prerequisites":['+prereq_id+']}}'
+        flag_result = update_session.patch(f"{url}/api/v1/challenges/{challenge_id}",json=json.loads(payload))
+    except Exception:
+      print("[e] Something's wrong, nothing changed.")
+
+def make_visible(challenge_id):
+  with requests.Session() as update_session:
+    update_session.headers.update({"Authorization": f"Token {token}"})
+    payload = '{"state":"visible"}'
     flag_result = update_session.patch(f"{url}/api/v1/challenges/{challenge_id}",json=json.loads(payload))
-    if flag_result.status_code > 399:
-      print('[e] Challenge '+challenge_id+' points not updated')
+
+def patch_new_prereq():
+  challenge_dict = {}
+  cid = 82
+  while True:
+    try:
+      with requests.Session() as check_existence:
+        check_existence.headers.update({"Authorization": f"Token {token}"})
+        challenge_result = check_existence.get(f"{url}/api/v1/challenges/{cid}",json='').json()
+        # print(challenge_result)
+        challenge_id = str(challenge_result['data']['id'])
+        challenge_name = challenge_result['data']['name']
+        challenge_value = str(challenge_result['data']['value'])
+        challenge_category = challenge_result['data']['category']
+        challenge_prereq = check_req(challenge_id)
+        # print(challenge_id+": ['"+challenge_name+"','"+challenge_value+"','"+challenge_category+"','"+challenge_prereq+"'],")
+        challenge_dict[challenge_id]=[challenge_name,challenge_value,challenge_category,challenge_prereq]
+    except Exception:
+      break
+    cid+=1
+
+  birth_month1_id = ''
+  for key,value in challenge_dict.items():
+    if "Birth Month 1" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq(key,"79")
+        birth_month1_id = key
+        make_visible(key)
+    elif "XOR Challenge 8" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq(key,"35")
+        make_visible(key)
+    elif "XOR Challenge 7" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq(key,"44")
+        make_visible(key)
+    elif "XOR Challenge 4" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq(key,"64")
+        make_visible(key)
+    elif "Birth Month 2" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq(key,"38")
+        make_visible(key)
+    elif "XOR Challenge 5" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq("31",key)
+        make_visible (31)
+        patch_prereq(key,"22")
+        make_visible(key)
+    elif "XOR Challenge 6" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq("11",key)
+        make_visible(11)
+        patch_prereq(key,"22")
+        make_visible(key)
+    elif "XOR Challenge 1" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq("80",key)
+        make_visible(80)
+        patch_prereq(key,"77")
+        make_visible(key)
+    elif "XOR Challenge 2" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq("71",key)
+        make_visible(71)
+        patch_prereq("47",key)
+        make_visible (47)
+        patch_prereq(key,"77")
+        make_visible(key)
+    elif "XOR Challenge 3" in value:
+      if challenge_dict.get(key)[3] == '':
+        patch_prereq("81",key)
+        make_visible(81)
+        patch_prereq("12",key)
+        make_visible(12)
+        patch_prereq(key,birth_month1_id)
+        make_visible(key)
+
+
+#   if check_name("XOR Challenge 3") == True and check_name("Birth Month 1") == True:
+#     patch_prereq_name("XOR Challenge 3","Birth Month 1")
+#   else:
+#     pass
+
 
 if __name__ == "__main__":
   token = "15c288f2a166e3cef2ebb182a007212e747947aae7ff55fe103bcbb7f1695e2a"
   url = "http://209.114.126.86"
   
-  # cid,cstatus = challenge_id_existance('xor',"8")
-  # print(cid)
-  # print(cstatus)
-  count_coordination(2)
+  patch_new_prereq()
