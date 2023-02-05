2946275593cc3f8eea0ce04d9b550678ff4ef3fc
jackiezkl <matins.fluting_0t@icloud.com>
2023-02-05 10:54:55 -0600

    update

--- qa.py
+++ qa.py
@@ -3,12 +3,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 import time,random,string
 
+while True:
+  try:
+    userinput = input("Please enter how many user do you want to create?")
+    number = int(userinput)
+    break
+  except Exception:
+    continue
+
 firefoxoptions = Options()
 firefoxoptions.add_argument("--headless")
 
 with webdriver.Firefox(options=firefoxoptions) as driver:
   driver.get("http://127.0.0.1")
-  for i in range(1,16):
+  for i in range(1,number):
     username = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=5))
     email = f"{username}@gmail.com"
     fullname = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=5))+" "+''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=5))
